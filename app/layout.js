68d2ffae7d2f8c2d932287a7d4febb3d9b0a1fcb
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-13 10:44:29 -0700

    first commit

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "ResumeAlign",
+  description: "Align resume with job expectation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
