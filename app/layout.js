297dd88616abf43fffd73ff5cacc9aac66e32469
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-13 11:36:29 -0700

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,13 @@
 import "./globals.css";
-import {NextUIProvider} from '@nextui-org/react'
 export const metadata = {
   title: "ResumeAlign",
   description: "Align resume with job expectation",
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
-    <NextUIProvider>
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
-    </NextUIProvider>
   );
 }
