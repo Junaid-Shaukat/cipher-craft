6bc1c056fd05bb3c5c5ac1f026102facec6ecc87
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-13 10:54:35 -0700

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Footer from '@/components/Home/Footer'
+import Header from '@/components/Home/Header'
+import Hero from '@/components/Home/Hero'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <Header/>
+      <Hero/>
+      <Footer/>
     </div>
   )
 }
